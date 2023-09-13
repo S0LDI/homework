5bc6398dc652bdc63557dec4c0c3bb2efcee1908
S0LDI GOLD <denysk06@gmail.com>
2023-09-13 21:43:17 +0000

    №7

---
+++ Заняття 3. №7/Заняття 3. №7.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "ukr");
+    double number;
+
+
+   
+    cout << "Введіть число: ";
+    cin >> number;
+
+    
+    double square = number * number;
+    double cube = number * number * number;
+
+    
+    cout << "Квадрат числа " << number << " дорівнює " << square << endl;
+    cout << "Куб числа " << number << " дорівнює " << cube << endl;
+
+    return 0;
+}
+
