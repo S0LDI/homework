a0adf099edb1889bf0b99ea86ffcebc1a7a9e6da
S0LDI GOLD <denysk06@gmail.com>
2023-09-13 20:21:35 +0000

    задание №1

---
+++ Заняття 3. №1/Заняття 3. №1.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << "multiplication: " << a * b << endl << "addition: " << a + b << endl;
+    return 0;
+}
+
+
