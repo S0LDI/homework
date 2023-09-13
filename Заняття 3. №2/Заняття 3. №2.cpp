dbfaa519bbcde998ca67c26ad196816e588a1bb1
S0LDI GOLD <denysk06@gmail.com>
2023-09-13 20:36:06 +0000

    задание №2

---
+++ Заняття 3. №2/Заняття 3. №2.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    double kilometers;
+    cout << "Enter the number of kilometers" << endl;
+    cin >> kilometers;
+    double meters = kilometers * 1000;
+    cout << "kilometers converted to meters" << meters  << endl;
+    
+
+}
+
