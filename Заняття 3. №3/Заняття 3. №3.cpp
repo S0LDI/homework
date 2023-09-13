72f5e47a9c335759ef46cdfed3321163630acd04
S0LDI GOLD <denysk06@gmail.com>
2023-09-13 21:03:36 +0000

    №3

---
+++ Заняття 3. №3/Заняття 3. №3.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int numberofminutes;
+    cout << "Do you want to know how much your call will cost?" << endl;
+    cin >> numberofminutes;
+    float costperminute = numberofminutes * 0.4;
+    cout << "the price of your call will cost" << costperminute<<"$" << endl;
+
+
+   
+}
+
+
