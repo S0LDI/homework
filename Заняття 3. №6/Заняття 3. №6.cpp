f335602f250d81743d29e57d560bf7c8144c57bd
S0LDI GOLD <denysk06@gmail.com>
2023-09-13 21:35:52 +0000

    №6

---
+++ Заняття 3. №6/Заняття 3. №6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "ukr");
+    const double marsOrbitalPeriodEarthDays = 686.0;   
+
+    double marsOrbits; 
+
+    cout << "Введіть кількість обертів Марсу навколо Сонця: ";
+    cin >> marsOrbits;
+
+    
+    double marsYears = marsOrbits / marsOrbitalPeriodEarthDays;
+
+    cout << "Кількість земних років, відповідних " << marsOrbits << " обертам Марсу: " << marsYears << " років" << endl;
+
+    return 0;
+}
+
