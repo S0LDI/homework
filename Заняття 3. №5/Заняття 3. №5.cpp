bc27bbab222eab9f49980a7de0cc12a77fa6c543
S0LDI GOLD <denysk06@gmail.com>
2023-09-13 21:22:44 +0000

    №5

---
+++ Заняття 3. №5/Заняття 3. №5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "ukr");
+
+    const double euroRate = 31.0;  
+    const double dollarRate = 27.0;  
+    const double zlotyRate = 7.0;  
+
+    double amountInHryvnia;
+    cout << "Введіть суму в гривнях: ";
+    cin >> amountInHryvnia;
+
+    
+    double amountInEuro = amountInHryvnia / euroRate;
+    double amountInDollars = amountInHryvnia / dollarRate;
+    double amountInZloty = amountInHryvnia / zlotyRate;
+
+    cout << "Сума в євро: " << amountInEuro << endl;
+    cout << "Сума в доларах: " << amountInDollars << endl;
+    cout << "Сума в злотих: " << amountInZloty << endl;
+
+    return 0;
+}
