bad1ce439fea7d4d88403f3a7cf16afcb6a66b0c
S0LDI GOLD <denysk06@gmail.com>
2023-09-22 12:21:50 +0000

    №6

---
+++ Заняття 4. №6/Заняття 4. №6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "ukr");
+    int number;
+
+    cout << "Введіть двоцифрове число: ";
+    cin >> number;
+
+    
+    int num1 = number / 10;  
+    int num2 = number % 10;  
+
+    if (num1 == num2) {
+        cout << "Число має однакові цифри" << endl;
+    }
+    else {
+        cout << "Число не має однакових цифр" << endl;
+    }
+
+    return 0;
+}
+
