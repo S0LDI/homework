52e4684b963ed70e4ca51dbe9f5ecd894ce78672
S0LDI GOLD <denysk06@gmail.com>
2023-09-22 13:28:34 +0000

    №10

---
+++ Заняття 4. №10/Заняття 4. №10.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int num1, num2;
+
+    
+    std::cout << "Введіть перше число: ";
+    std::cin >> num1;
+
+    std::cout << "Введіть друге число: ";
+    std::cin >> num2;
+
+   
+    if (num1 > num2) {
+        int тимчасова = num1;
+        num1 = num2;
+        num2 = тимчасова;
+    }
+
+   
+    std::cout << "Значення змінних після обміну:" << std::endl;
+    std::cout << "Перше число: " << num1 << std::endl;
+    std::cout << "Друге число: " << num2 << std::endl;
+
+    return 0;
+}
