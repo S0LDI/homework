4f081320d5ad58e859806a256b4143bde948c45d
S0LDI GOLD <denysk06@gmail.com>
2023-10-02 08:56:32 +0000

    №6

---
+++ Заннятя 5. №6/Заннятя 5. №6.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    int count = 0;   
+    int sum = 0;     
+
+    
+    while (count < 10) {
+        int number;
+        std::cout << "Введіть число " << (count + 1) << ": ";
+        std::cin >> number;
+
+        sum += number;  
+        count++;       
+    }
+
+    
+    double average = static_cast<double>(sum) / 10;
+
+    std::cout << "Сума введених чисел: " << sum << std::endl;
+    std::cout << "Середнє арифметичне: " << average << std::endl;
+
+    return 0;
+}
