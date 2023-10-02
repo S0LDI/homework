bbfa3c903453e642c928e390605b3744d4c6a976
S0LDI GOLD <denysk06@gmail.com>
2023-10-02 08:17:40 +0000

    #1

---
+++ Завдання 5. №1/Завдання 5. №1.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    double temperature;
+
+   
+    std::cout << "Введіть температуру: ";
+    std::cin >> temperature;
+
+    
+    if (temperature < 35.0) {
+        std::cout << "Понижена температура, стан ослабленого організму" << std::endl;
+    }
+    else if (temperature >= 35.0 && temperature <= 36.0) {
+        std::cout << "Понижена температура" << std::endl;
+    }
+    else if (temperature > 36.6 && temperature <= 36.9) {
+        std::cout << "Нормальна температура, людина здорова" << std::endl;
+    }
+    else {
+        std::cout << "Ви ввели невірну температуру" << std::endl;
+    }
+
+    return 0;
+}
+
