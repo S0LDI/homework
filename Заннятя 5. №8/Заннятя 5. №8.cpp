3a79f0e0ab3107b5fcba5b784a3da03480e65c6b
S0LDI GOLD <denysk06@gmail.com>
2023-10-02 09:14:40 +0000

    №8

---
+++ Заннятя 5. №8/Заннятя 5. №8.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    int sum = 0;
+    int count = 0;
+
+    do {
+        int number;
+        std::cout << "Введіть число: ";
+        std::cin >> number;
+
+        sum += number;
+        count++;
+
+    } while (count < 7);
+
+    std::cout << "Сума семи введених чисел: " << sum << std::endl;
+
+    return 0;
+}
