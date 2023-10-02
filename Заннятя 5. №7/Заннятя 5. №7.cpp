3ea7d6274a9417c1081e6cd924805e195166224a
S0LDI GOLD <denysk06@gmail.com>
2023-10-02 09:05:37 +0000

    №7

---
+++ Заннятя 5. №7/Заннятя 5. №7.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    int n;
+    std::cout << "Введіть n: ";
+    std::cin >> n;
+    int sum = 0;
+    int count = 0;
+    int currentNumber = 1;
+    do {
+        if (currentNumber % 2 != 0) {
+            std::cout << "Непарне число: " << currentNumber << ", Квадрат: " << currentNumber * currentNumber << std::endl; 
+            sum += currentNumber * currentNumber;
+            count++;
+        }      
+        currentNumber++;
+
+    } while (count < n);
+
+    std::cout << "Сума квадратів перших " << n << " непарних чисел: " << sum << std::endl;
+
+    return 0;
+}
