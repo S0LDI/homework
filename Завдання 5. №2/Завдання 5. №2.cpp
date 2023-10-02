d2e8995bbf61e877f00d5c2bc750c6c5082e1775
S0LDI GOLD <denysk06@gmail.com>
2023-10-02 08:24:45 +0000

    №2

---
+++ Завдання 5. №2/Завдання 5. №2.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    int hours, minutes, seconds;
+
+   
+    std::cout << "Введіть години: ";
+    std::cin >> hours;
+
+    std::cout << "Введіть хвилини: ";
+    std::cin >> minutes;
+
+    std::cout << "Введіть секунди: ";
+    std::cin >> seconds;
+
+  
+    if (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60 && seconds >= 0 && seconds < 60) {
+        std::cout << "Час є допустимим." << std::endl;
+    }
+    else {
+        std::cout << "Час недопустимий." << std::endl;
+    }
+
+    return 0;
+}
+
