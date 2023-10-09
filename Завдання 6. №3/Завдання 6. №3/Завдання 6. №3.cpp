29ec418a3de16c64811c2f4a240e237839ddd226
soldi <denysk06@gmail.com>
2023-10-09 15:19:35 +0300

    №3

---
+++ Завдання 6. №3/Завдання 6. №3/Завдання 6. №3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    long arr[7] = { 1, 2, 3, 4, 5, 6, 7 };
+    long sum = 0;
+    for (int i = 0; i < 7; i++) {
+        if (arr[i] % 2 == 0) {
+            sum += arr[i];
+        }
+    }
+    cout << "Сума парних елементів масиву: " << sum << endl;
+    return 0;
+}
