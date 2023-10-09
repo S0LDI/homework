bf9b67a73c8df6ea3c7ba2d4ca6cb83138cb1cca
soldi <denysk06@gmail.com>
2023-10-09 15:26:02 +0300

    №4

---
+++ Завдання 6. №4/Завдання 6. №4/Завдання 6. №4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    int arr[30];
+    for (int i = 0; i < 30; i++) {
+        arr[i] = pow(2, i);
+    }
+
+    cout << "Елементи масиву у прямому порядку: ";
+    for (int i = 0; i < 30; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl << "Елементи масиву у зворотньому порядку: ";
+    for (int i = 29; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
