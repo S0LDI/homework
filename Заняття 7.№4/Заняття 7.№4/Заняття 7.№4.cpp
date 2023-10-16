b5306b35c3a911f03f81880a3b9def60eaad2c8f
soldi <denysk06@gmail.com>
2023-10-16 16:14:00 +0300

    №4

---
+++ Заняття 7.№4/Заняття 7.№4/Заняття 7.№4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    int n;
+    std::cout << "Введіть розмір масиву: ";
+    std::cin >> n;
+    std::vector<int> a(n);
+
+    std::cout << "Введіть елементи масиву: ";
+    for (int i = 0; i < n; i++)
+        std::cin >> a[i];
+
+    if (a[0] % 2 == 0) {
+        for (int i = 0; i < n; i++)
+            if (a[i] % 2 != 0)
+                std::cout << a[i] << " ";
+    }
+    else {
+        for (int i = 0; i < n; i++)
+            if (a[i] % 2 == 0)
+                std::cout << a[i] << " ";
+    }
+
+    return 0;
+}
