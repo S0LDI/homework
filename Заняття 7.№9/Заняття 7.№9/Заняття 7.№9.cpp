06c6421190238276fad91c65bb9467d867b9cf98
soldi <denysk06@gmail.com>
2023-10-16 16:37:24 +0300

    №9

---
+++ Заняття 7.№9/Заняття 7.№9/Заняття 7.№9.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+std::string encryptString(const std::string& input) {
+    std::string result = input;
+
+    for (char& ch : result) {
+        if (std::isalpha(ch)) { 
+            char base = islower(ch) ? 'a' : 'A'; 
+            ch = (ch - base + 1) % 26 + base; 
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    setlocale(LC_ALL,"ukr");
+    std::string inputString;
+    std::cout << "Введіть рядок: ";
+    std::getline(std::cin, inputString);
+
+    std::string encryptedString = encryptString(inputString);
+
+    std::cout << "Зашифрований рядок: " << encryptedString << std::endl;
+
+    return 0;
+}
