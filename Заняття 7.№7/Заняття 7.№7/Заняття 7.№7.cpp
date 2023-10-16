fdba8ce9bef094cf535e66d1001983a5629ff100
soldi <denysk06@gmail.com>
2023-10-16 16:31:48 +0300

    №7

---
+++ Заняття 7.№7/Заняття 7.№7/Заняття 7.№7.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    std::string inputString;
+    std::cout << "Введіть рядок: ";
+    std::cin >> inputString;
+
+    for (int i = 0; i < inputString.length(); i++) {
+        if (inputString[i] == '+') {
+            inputString[i] = '-'; 
+        }
+    }
+
+    std::cout << "Результат: " << inputString << std::endl;
+
+    return 0;
+}
