7e92cf12754a71b23b63966d3a4bdb79d6597476
soldi <denysk06@gmail.com>
2023-10-16 16:22:16 +0300

    №5

---
+++ Заняття 7.№5/Заняття 7.№5/Заняття 7.№5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    int n;
+    std::cout << "Введіть розмір масиву: ";
+    std::cin >> n;
+    int a[100]; 
+    int newSize = 0;
+
+    std::cout << "Введіть елементи масиву: ";
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[newSize];
+        if (a[newSize] % 2 == 0) {
+            newSize++; 
+        }
+    }
+
+    std::cout << "Масив після видалення непарних чисел: ";
+    for (int i = 0; i < newSize; i++) {
+        std::cout << a[i] << " ";
+    }
+
+    return 0;
+}
