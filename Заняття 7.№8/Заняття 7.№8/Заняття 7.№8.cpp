5f8f09ad3618cc99424b25ff7c1b11bbd02db336
soldi <denysk06@gmail.com>
2023-10-16 16:34:21 +0300

    №8

---
+++ Заняття 7.№8/Заняття 7.№8/Заняття 7.№8.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+int countDigitsInString(const std::string& str) {
+    int count = 0;
+
+    for (char ch : str) {
+        if (std::isdigit(ch)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+    std::string inputString;
+    std::cout << "Введіть рядок: ";
+    std::cin >> inputString;
+
+    int digitCount = countDigitsInString(inputString);
+
+    std::cout << "Кількість цифр у рядку: " << digitCount << std::endl;
+
+    return 0;
+}
