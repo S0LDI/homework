550838aab748592a28cf010986c26f9a85b816f2
soldi <denysk06@gmail.com>
2023-10-16 15:58:02 +0300

    №2

---
+++ Заняття 7.№2/Заняття 7.№2/Заняття 7.№2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "ukr");
+   
+    int* dynamicInt = new int(10);
+    double* dynamicDouble = new double(5.5);
+    char* dynamicChar = new char('A');
+
+
+    std::cout << "dynamicInt: " << *dynamicInt << std::endl;
+    std::cout << "dynamicDouble: " << *dynamicDouble << std::endl;
+    std::cout << "dynamicChar: " << *dynamicChar << std::endl;
+
+    
+    double product = *dynamicInt * *dynamicDouble;
+
+   
+    std::cout << "Добуток dynamicInt і dynamicDouble: " << product << std::endl;
+
+    
+    double average = (*dynamicInt + *dynamicDouble + *dynamicChar) / 3.0;
+
+  
+    std::cout << "Середнє арифметичне: " << average << std::endl;
+
+  
+    delete dynamicInt;
+    delete dynamicDouble;
+    delete dynamicChar;
+
+    return 0;
+}
